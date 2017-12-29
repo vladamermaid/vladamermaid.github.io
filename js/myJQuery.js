7dc98c497ee21c39a1a97538c774dff7ab92d24b
KostyaOvs <mobikostya@gmail.com>
2017-12-29 23:39:55 +0200

    2 step
    the gallery of my doter

---
+++ js/myJQuery.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+
+});
+
+
+
